c622dcd4db63ec654b4d9adf2b987fab390598b7
Simon Gonzalez <ximonali@gmail.com>
2017-10-31 20:16:18 -0400

    root page changed and adding sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 import { TabsPage } from '../pages/tabs/tabs';
+import { SettingsPage } from '../pages/settings/settings';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  @ViewChild('nav') nav: NavController;
+  tabsPage:any = TabsPage;
+  settingsPage:any = SettingsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuController: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,5 +23,12 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  onLoad(page: any){
+    this.nav.setRoot(page);
+    this.menuController.close();
+  }
+
+
 }
 
